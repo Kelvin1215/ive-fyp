af376260684425f0ed578b4ce15b9755bb136e9a
Kelvin啊希 <kahei1215.hk@gmail.com>
2025-01-28 00:07:58 +0800

    update the form searching,  the ordering of form field and table column

--- admin/src/page/Menu/index.js
+++ admin/src/page/Menu/index.js
@@ -1,31 +1,32 @@
 import React, { useEffect, useState } from "react";
-import { Layout, Button, Flex, Tooltip, Form, Input, Radio } from "antd";
+import {
+  Layout,
+  Button,
+  Flex,
+  Tooltip,
+  Form,
+  Input,
+  Radio,
+  Table,
+  Typography,
+} from "antd";
 import { ClearOutlined } from "@ant-design/icons";
+import { useNavigate } from "react-router-dom";
+import intl from "react-intl-universal";
 import Sider from "../layout/Sider";
 import Footer from "../layout/Footer";
 import Header from "../layout/Header";
 import { getDishesType } from "../../api/DishesType";
-import intl from "react-intl-universal";
-import { useNavigate } from "react-router-dom";
-import "./index.css";
 
-function Menu() {
-  return (
-    <Layout style={{ minHeight: "100vh" }}>
-      <Sider />
-      <Layout>
-        <Header />
-        <Content />
-        <Footer />
-      </Layout>
-    </Layout>
-  );
-}
+import "./index.css";
+import { findInMenu } from "../../api/Menu";
 
 function Content() {
   const [dishesType, setDishesType] = useState([]);
   const navigator = useNavigate();
-  const [form] = Form.useForm(); // 確保 form 已正確初始化
+  const [form] = Form.useForm();
+  const [data, setData] = useState([]);
+  const [type, setType] = useState("");
 
   useEffect(() => {
     getDishesType().then((res) => {
@@ -39,6 +40,13 @@ function Content() {
                 color="primary"
                 variant="outlined"
                 style={{ flex: 1, height: "100%", fontSize: "16px" }}
+                onClick={() => {
+                  setType(item.name_Zh_HK);
+                  findInMenu("", "", "", "", "", item.id).then((res) => {
+                    setData(res.data);
+                  });
+                  form.setFieldsValue({ type: item.id });
+                }}
               >
                 {item.name_Zh_HK}
               </Button>
@@ -55,6 +63,13 @@ function Content() {
                 color="primary"
                 variant="outlined"
                 style={{ flex: 1, height: "100%", fontSize: "16px" }}
+                onClick={() => {
+                  setType(item.name_Us_En);
+                  findInMenu("", "", "", "", "", item.id).then((res) => {
+                    setData(res.data);
+                  });
+                  form.setFieldsValue({ type: item.id });
+                }}
               >
                 {item.name_Us_En}
               </Button>
@@ -63,74 +78,218 @@ function Content() {
         );
       }
     });
-  }, [navigator]);
+  }, [navigator, form]);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await findInMenu("", "", "", "", "", "");
+        response.data.forEach((item) => {
+          item.key = item.id;
+        });
+        setData(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  const formFields = () => {
+    if (localStorage.getItem("locale") === "zh-HK") {
+      return (
+        <>
+          <Form.Item label={intl.get("name")} name="name_zh_HK">
+            <Input size="large" id="name_zh_HK" />
+          </Form.Item>
+          <Form.Item label={intl.get("nameCN")} name="name_zh_CN">
+            <Input size="large" id="name_zh_CN" />
+          </Form.Item>
+          <Form.Item label={intl.get("nameEN")} name="name_en_US">
+            <Input size="large" id="name_en_US" />
+          </Form.Item>
+        </>
+      );
+    } else {
+      return (
+        <>
+          <Form.Item label={intl.get("nameEN")} name="name_en_US">
+            <Input size="large" id="name_en_US" />
+          </Form.Item>
+          <Form.Item label={intl.get("name")} name="name_zh_HK">
+            <Input size="large" id="name_zh_HK" />
+          </Form.Item>
+          <Form.Item label={intl.get("nameCN")} name="name_zh_CN">
+            <Input size="large" id="name_zh_CN" />
+          </Form.Item>
+        </>
+      );
+    }
+  };
+
+  const tableColumns = () => {
+    if (localStorage.getItem("locale") === "zh-HK") {
+      return [
+        {
+          title: intl.get("name"),
+          dataIndex: "name_zh_HK",
+          key: "name_zh_HK",
+          sorter: (a, b) => a.name_zh_HK.localeCompare(b.name_zh_HK),
+        },
+        {
+          title: intl.get("nameCN"),
+          dataIndex: "name_zh_CN",
+          key: "name_zh_CN",
+          sorter: (a, b) => a.name_zh_CN.localeCompare(b.name_zh_CN),
+        },
+        {
+          title: intl.get("nameEN"),
+          dataIndex: "name_en_US",
+          key: "name_en_US",
+          sorter: (a, b) => a.name_en_US.localeCompare(b.name_en_US),
+        },
+        {
+          title: intl.get("price"),
+          dataIndex: "price",
+          key: "price",
+          sorter: (a, b) => a.price - b.price,
+        },
+        {
+          title: intl.get("status"),
+          dataIndex: "onSale",
+          key: "onSale",
+          sorter: (a, b) => (a.onSale > b.onSale ? 1 : -1),
+          render: (onSale) => {
+            return onSale === "Y" ? intl.get("active") : intl.get("inactive");
+          },
+        },
+      ];
+    } else {
+      return [
+        {
+          title: intl.get("nameEN"),
+          dataIndex: "name_en_US",
+          key: "name_en_US",
+          sorter: (a, b) => a.name_en_US.localeCompare(b.name_en_US),
+        },
+        {
+          title: intl.get("name"),
+          dataIndex: "name_zh_HK",
+          key: "name_zh_HK",
+          sorter: (a, b) => a.name_zh_HK.localeCompare(b.name_zh_HK),
+        },
+        {
+          title: intl.get("nameCN"),
+          dataIndex: "name_zh_CN",
+          key: "name_zh_CN",
+          sorter: (a, b) => a.name_zh_CN.localeCompare(b.name_zh_CN),
+        },
+
+        {
+          title: intl.get("price"),
+          dataIndex: "price",
+          key: "price",
+          sorter: (a, b) => a.price - b.price,
+        },
+        {
+          title: intl.get("status"),
+          dataIndex: "onSale",
+          key: "onSale",
+          sorter: (a, b) => (a.onSale > b.onSale ? 1 : -1),
+          render: (onSale) => {
+            return onSale === "Y" ? intl.get("active") : intl.get("inactive");
+          },
+        },
+      ];
+    }
+  };
   const onReset = () => {
     form.resetFields();
+    findInMenu("", "", "", "", "", "").then((res) => {
+      setData(res.data);
+    });
+    setType("");
   };
+
+
   return (
     <Layout.Content
       style={{
         margin: "12px",
         padding: 12,
+        overflow: "auto",
       }}
     >
       <Flex wrap gap="middle">
         {dishesType}
       </Flex>
       <Flex wrap gap="middle">
-        <Form form={form} name="form" layout="vertical">
+        <Form
+          form={form}
+          name="form"
+          layout="vertical"
+        >
           <Flex wrap gap={20} justify="center">
-            <Form.Item
-              label={intl.get("name")}
-              name="name_zh_HK"
-            >
-              <Input size="large" id="name_zh_HK" />
-            </Form.Item>
-            <Form.Item
-              label={intl.get("nameCN")}
-              name="name_zh_CN"
-            >
-              <Input size="large" id="name_zh_CN" />
-            </Form.Item>
-            <Form.Item
-              label={intl.get("nameEN")}
-              name="name_en_US"
-            >
-              <Input size="large" id="name_en_US" />
-            </Form.Item>
-            <Form.Item
-              label={intl.get("price")}
-              name="price"
-              style={{ order: 4 }}
-            >
+            {formFields()}
+            <Form.Item label={intl.get("price")} name="price">
               <Input size="large" id="price" />
             </Form.Item>
-            <Form.Item
-              label={intl.get("status")}
-              name="onSale"
-              style={{ order: 5 }}
-            >
+            <Form.Item label={intl.get("status")} name="onSale">
               <Radio.Group id="onSale">
                 <Radio value="Y">{intl.get("active")}</Radio>
                 <Radio value="N">{intl.get("inactive")}</Radio>
               </Radio.Group>
             </Form.Item>
-            <Form.Item label="&nbsp;" style={{ order: 6 }}>
+            <Form.Item label="&nbsp;">
               <Button icon={<ClearOutlined />} onClick={() => onReset()}>
                 {intl.get("reset")}
               </Button>
             </Form.Item>
-            <Form.Item label="&nbsp;" style={{ order: 7 }}>
+            <Form.Item label="&nbsp;">
               <Button type="primary" htmlType="submit">
                 {intl.get("submit")}
               </Button>
             </Form.Item>
+            <Form.Item name="type" hidden>
+              <Input />
+            </Form.Item>
           </Flex>
         </Form>
+      </Flex>
+      {type === "" ? null : (
+        <Typography.Title level={3}>{type}</Typography.Title>
+      )}
+      <Flex wrap gap="middle">
+        <Table
+          dataSource={Array.from(data)}
+          name="table"
+          scroll={{
+            y: 400,
+          }}
+          virtual
+          rowKey={(record) => record.key}
+          columns={tableColumns()}
+          pagination={{
+            showSizeChanger: true,
+            pageSizeOptions: ["10", "25", "50", "100"],
+            defaultPageSize: 10,
+          }}
+        />
       </Flex>
     </Layout.Content>
   );
 }
 
+function Menu() {
+  return (
+    <Layout style={{ minHeight: "100vh" }}>
+      <Sider />
+      <Layout>
+        <Header />
+        <Content />
+        <Footer />
+      </Layout>
+    </Layout>
+  );
+}
 export default Menu;
